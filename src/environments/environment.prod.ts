2f85303eda450655872dba8f6a11ef1325094448
Leo7camilo <59704656+Leo7camilo@users.noreply.github.com>
2022-06-19 20:08:43 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   apiUrl: 'https://delfos-api.herokuapp.com',
-  tokenAllowedDomains: [ /algamoney-api.herokuapp.com/ ],
-  tokenDisallowedRoutes: [/\/oauth2\/token/],
+  tokenAllowedDomains: [ /delfos-api.herokuapp.com/ ],
+  tokenDisallowedRoutes: [/\/oauth\/token/],
   oauthCallbackUrl: 'https://delfos-api.herokuapp.com',
   logoutRedirectToUrl: 'https://delfos-api.herokuapp.com'
 };
